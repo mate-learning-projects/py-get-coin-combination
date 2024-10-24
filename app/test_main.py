251c8ff6e6502723d2b9ff1376816db7a0ef2661
Oleksii Ivanov <oleks4335@gmail.com>
2024-10-24 15:15:06 +0200

    Add Coin combination solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,18 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, expected",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (25, [0, 0, 0, 1]),
+        (67, [2, 1, 1, 2]),
+        (4, [4, 0, 0, 0]),
+    ],
+)
+def test_get_coin_combination(cents, expected):
+    assert get_coin_combination(cents) == expected
